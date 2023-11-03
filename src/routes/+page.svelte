40587d2d71ab74ac29fd3bad4299d2bd57de6da2
Eric <eric.dezwart@live.at>
2023-11-03 04:39:49 +0100

    started adding ethersjs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,35 +1,69 @@
 <script>
-import {nomo} from "nomo-webon-kit";
+    import fish from "../assets/fish.webp"
+    import {zscSigner} from "ethersjs-nomo-webons";
+    import {ethers} from "ethers";
+    import {chainIdZeniqSmartChain, fish_address} from "../utils/constants.js";
+    import abi from '../abi/contracts_Fish_sol_Fish.json'
+    import {nomo} from "nomo-webon-kit";
 
-let res = {}
-</script>
+    const acc1 = '0x15686f45f6143334cf79f808D0eC04914541997F'
+    const acc2 = '0xfc8826ec9a88a8269F84988f0b6EC35bC21fc623'
 
-Hello
+    export async function getTx() {
+        return {
+            gasLimit: 21000,
+            gasPrice: 10000000000,
+            nonce: await zscSigner.getNonce(),
+            chainId: chainIdZeniqSmartChain,
+            type: 0,
+        };
+    }
 
 
-<button on:click={async () => {
+    const addCustomToken = async () => {
         try {
-            res = await nomo.addCustomToken({
-                contractAddress: '0xb38809246fe6c3e0C086E3db8dB9744c9375204d',
-                network: 'ZENIQSMART'
+            await nomo.addCustomToken({
+                contractAddress: fish_address,
+                network: 'zeniqSmartChain',
             })
         } catch (e) {
             console.log(e)
-            res = e
         }
-        try {
-            res = await nomo.getInstalledWebOns()
-        } catch (e) {
-            console.log(e)
-            res = e
-        }
-}}>
-    Add Custom Token
-</button>
+    }
 
-{JSON.stringify(res)}
+</script>
+
+<div class="page">
+    <button on:click={async () => {
+         const contract = new ethers.Contract(fish_address, abi, zscSigner);
+         const tx = await contract.buy({ value: ethers.parseEther("1.0"), ...await getTx() });
+         const receipt = await tx.wait()
+         console.log(receipt)
+    }}>
+        <img src={fish} alt="">
+    </button>
+    <button on:click={async () => {
+         await addCustomToken()
+    }}>
+        <img src={fish} alt="">
+    </button>
+</div>
 
 <style>
+    .page {
+        justify-content: center;
+        gap: 20px;
+    }
 
+    button {
+        border: 2px solid #487cc3;
+        background: #d6e2ef;
+        padding: 40px;
+        border-radius: 20px;
+        aspect-ratio: 1;
+    }
 
+    img {
+        width: 200px;
+    }
 </style>
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+export const chainIdZeniqSmartChain = 383414847825;
+
+export const fish_address = "0x626e2744e713bb7C9845C6c2AAC10493D8874CAb"
